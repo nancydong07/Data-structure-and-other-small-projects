4a4bab8397a95c0ba378e0e6ea2a26145aa07dc9
nancydong0706 <nancydong0706@users.noreply.github.com>
2020-06-30 02:56:47 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -10,12 +10,9 @@ if len(argv) != 2:
 
 data = db.execute("SELECT first, middle, last, birth FROM students WHERE house =? ORDER BY last, first", argv[1])
 
-counter = 0
-ll = []
 for rows in data:
     for i,u in rows.items():
         ll.append(rows[i])
-        counter = counter + 1
         if (rows[i] == rows["birth"]):
             if (rows["middle"] == None):
                 print(ll[0], ll[2], end = "")
